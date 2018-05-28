97226602cff41bca0c0ccda86b7bde6f8c101c21
songjian <songjian0609@qq.com>
2018-05-28 16:32:06 +0800

    junit

---
+++ src/test/java/wang/raye/admin/service/impl/MenuServiceImplTest.java
@@ -0,0 +1,29 @@
+package wang.raye.admin.service.impl;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import wang.raye.admin.model.Menu;
+import wang.raye.admin.service.MenuService;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2018/5/28.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MenuServiceImplTest {
+    @Autowired
+    private MenuService menuService;
+
+    @Test
+    public void selectAll() throws Exception {
+        List<Menu> menuList =  menuService.selectAll();
+        Assert.assertEquals(menuList.size(),3);
+    }
+
+}
