fb412ba357e6d99f38ac729e2090b510730b1a88
songjian <songjian0609@qq.com>
2018-08-24 15:23:33 +0800

    打包

--- src/main/java/wang/raye/admin/Application.java
+++ src/main/java/wang/raye/admin/Application.java
@@ -3,6 +3,7 @@ package wang.raye.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
@@ -19,6 +20,12 @@ public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class);
 
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// 注意这里要指向原先用main方法执行的Application启动类
+		return builder.sources(Application.class);
 	}
 
 }
